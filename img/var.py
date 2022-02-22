846658798ae8eb33376f003ba2e343944b099e7e
Diego Lago Paz <80002149+PanicoEstaCogido123@users.noreply.github.com>
2022-02-22 12:59:55 +0100

    Add files via upload

--- img/var.py
+++ img/var.py
@@ -4,7 +4,7 @@ from main import FileDialogAbrir
 from window import Ui_MainWindow
 from windowcal import Ui_windowcal
 
-global ui, dlgaviso, dlgcalendar, dlgabrir, copia, msg, cv, btnfacdel
+global ui, dlgaviso, dlgcalendar, dlgabrir, copia, msg, cv, btnfacdel, cmbProducto, txtCantidad, precio, codpro
 filedb='bbdd.db'
 ui: Ui_MainWindow
 #dlgaviso: DialogoAviso
@@ -14,3 +14,10 @@ dlgAbrir: FileDialogAbrir
 '''
 otras variables
 '''
+from img import abrirCarpeta
+from img import bajar
+from img import calendario
+from img import imprimir
+from img import limpiar
+from img import salir
+from img import subir
